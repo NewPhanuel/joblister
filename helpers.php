a9eccddc0ccbfbe6511970721b2b9d79bdf2a0b2
Omamowho Phanuel <omamowhophanuel@gmail.com>
2024-05-26 12:20:53 +0100

    Initial commit and started home view

---
+++ helpers.php
@@ -0,0 +1,7 @@
+<?php
+declare(strict_types=1);
+
+function basePath(string $path): string
+{
+    return __DIR__ . '/' . $path;
+}
---
+++ public/index.php
@@ -0,0 +1,5 @@
+<?php
+declare(strict_types=1);
+
+require '../helpers.php';
+require basePath('/views/home.view.php');
