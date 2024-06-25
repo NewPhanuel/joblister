3e31512930ec56685af24de42587a1b376490d58
Omamowho Phanuel <omamowhophanuel@gmail.com>
2024-06-25 16:47:44 +0100

    Fixed some of the problems that occurred on the production environment

--- routes.php
+++ routes.php
@@ -21,6 +21,6 @@ $router->put('/listings', 'ListingsController@update', ['auth']);
 // Authentication routes
 $router->get('/auth/register', 'UserController@create', ['guest']);
 $router->get('/auth/login', 'UserController@login', ['guest']);
-$router->post('/auth/register', 'Usercontroller@store', ['guest']);
+$router->post('/auth/register', 'UserController@store', ['guest']);
 $router->post('/auth/logout', 'UserController@logout', ['auth']);
 $router->post('/auth/login', 'UserController@authenticate', ['guest']);
